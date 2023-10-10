508d233407d21faf4a23393bf2704d771be02897
extf8 <extfield8@gmail.com>
2023-10-10 12:32:08 +0200

    Added Book object mockup

---
+++ script.js
@@ -0,0 +1,13 @@
+const myLibrary = []
+
+function Book(author, title, numberOfPages, isRead ) {
+    // the constructor
+    this.author = author,
+    this.title = title,
+    this.numberOfPages = numberOfPages,
+    this.isRead = isRead
+}
+
+function addBookToLibrary() {
+    // do stuff here
+}
