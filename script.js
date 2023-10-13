261eac3821c750e7697c44c08d177f856cc9391d
extf8 <extfield8@gmail.com>
2023-10-13 05:28:36 +0200

    Added modal / TODO layout bug fix

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
-const myLibrary = []
+const myLibrary = [];
 
-function Book(author, title, numberOfPages, isRead ) {
+function Book(author, title, numberOfPages, isRead) {
     // the constructor
-    this.author = author,
-    this.title = title,
-    this.numberOfPages = numberOfPages,
-    this.isRead = isRead
+    (this.author = author),
+        (this.title = title),
+        (this.numberOfPages = numberOfPages),
+        (this.isRead = isRead);
 }
 
 function addBookToLibrary() {
     // do stuff here
 }
+
+const dialog = document.querySelector('dialog');
+const newBookBtn = document.getElementById('newBookBtn');
+const closeButton = document.querySelector('dialog button');
+
+// "Show the dialog" button opens the dialog modally
+newBookBtn.addEventListener('click', () => {
+    dialog.showModal();
+});
+
+// "Close" button closes the dialog
+closeButton.addEventListener('click', () => {
+    dialog.close();
+});
